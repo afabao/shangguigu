ead0d5a852ccb925b2634d4f2958173744b58a9f
afabao <421377215@qq.com>
2019-06-13 00:06:27 +0800

    eureka 1

--- microservicecloud-eureka-7001/src/main/java/com/afabao/shangguigu/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/afabao/shangguigu/EurekaServer7001_App.java
@@ -2,17 +2,17 @@ package com.afabao.shangguigu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author: 啊发包
- * @Date: 2019/06/10 2019-06-10
+ * @Date: 2019/06/12 2019-06-12
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider8001_App {
-
+@EnableEurekaServer
+public class EurekaServer7001_App {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class,args);
+        SpringApplication.run(EurekaServer7001_App.class,args);
     }
+
 }
